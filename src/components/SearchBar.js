3254f6e9af1dac073102118cc7bdfe2e7c6a3953
Andrew Gorman <andrewgorman101@gmail.com>
2020-08-25 17:56:24 +0100

    terminal-esque interface working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,11 +5,16 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
+        this.terminal = React.createRef();
+
         this.state = {
             cursor: '_',
-            nextLine: false
+            nextLine: false,
+            startType: false,
+            rows: []
         }
     }
+
     postAPI = (payload) => {
         console.log(process.env.REACT_APP_WIT_API_KEY)
         const headers = {
@@ -28,31 +33,22 @@ class SearchBar extends Component {
 
     handleSubmit= (e) => {
         e.preventDefault() 
-        console.log(e.target[0].value)
+        console.log(e.target.parentNode.parentNode)
         let payload = encodeURIComponent(e.target[0].value);
         this.postAPI(payload)
-    } 
+        
+        this.terminal.current.deleteRow(0)
 
-    inputLine = () => {
-        if (this.state.nextLine == true) {
-            return (
-                <form onSubmit={this.handleSubmit} method="POST">
-                    <i style={terminal} className="big angle right icon"></i>
-                    <input style={searchBar} type="text" required="text"></input>
-                    <button style={searchButton}>
-                        <i className="angle right icon"></i>
-                    </button>
-                </form>
-            )
-        } else {
-            return null
-        }
-    }
+        console.log(e.currentTarget.onfocus)
+
+
+        this.setState({ rows: [...this.state.rows, '0'] })
+    } 
 
     render() {
         return(
             <div style={container}>
-                <table>
+                <table ref={this.terminal}>
                     <tr>
                         <td>
                             <i style={terminal} className="big angle right icon"></i>
@@ -60,21 +56,43 @@ class SearchBar extends Component {
                         <td>
                             <Typewriter
                                 options={{
-                                    cursor: '_'
+                                    cursor: '',
                                 }}
                                 onInit={(typewriter) => {
                                     typewriter.typeString('Ask Me Anything, or simply say Hi!')
                                     .callFunction(() => {
                                         console.log('String typed out!');
-                                        this.setState({nextLine: true})
+                                        this.setState({ rows: [...this.state.rows, '0'] })
                                     })
                                     .start();
                                 }}
                             />
                         </td>
+                        <td>
+                            <h1 className={this.state.nextLine ? "hide": "blink cursor"}>_</h1>
+                        </td>
                     </tr>
+                    {
+                        this.state.rows.map((row) => {
+                            console.log('HELLO')
+                            return (
+                                <tr>
+                                    <td>
+                                        <i style={terminal} className="big angle right icon"></i>
+                                    </td>
+                                    <td>
+                                        <form onSubmit={this.handleSubmit} method="post">
+                                            <input style={searchBar} type="text" required="text" autofocus="autofocus" onfocus="this.select()"></input>
+                                        </form>
+                                    </td>
+                                    <td>
+                                        <i className="window minimize outline icon blink"></i>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
                 </table>
-                {this.inputLine()}
             </div>
         )
     }
@@ -89,12 +107,15 @@ const container = {
     transform: 'translateX(-12px)'
 }
 
+const tableStyle = {
+}
+
 const searchBar = {
-	width: '250px',
+	width: '100%',
 	borderRight: 'none',
 	marginRight: '5px',
 	padding: '5px',
-	height: '50px',
+	height: '30px',
 	border: '0px solid rgba(0, 0, 0, 0.27)',
 	backgroundColor: 'white',
 	color: 'white',
@@ -103,15 +124,15 @@ const searchBar = {
 	borderRadius: '5px 5px 5px 5px',
     mozTransition: 'all 0.3s',
 	transition: 'all 0.3s',
-
+    caretColor: 'transparent',
     fontFamily: 'monospace',
     fontSize: '1.5em',
 }
 
 const searchButton = {
     visibility: 'hidden',
-	width: '80px',
-    height: '50px',
+	width: '10px',
+    height: '10px',
 	border: '0px solid rgba(0, 0, 0, 0.27)',
 	textAlign: 'center',
 	borderRadius: '5px 5px 5px 5px',
